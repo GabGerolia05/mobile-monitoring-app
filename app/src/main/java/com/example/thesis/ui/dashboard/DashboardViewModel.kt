b00a373cf97f354f451f4dbe339100c52e3cf208
GEROLIA_JohnGabriel_BSIT32E1 <johngabrielgerolia05@gmail.com>
2024-08-19 18:57:57 +0800

    first commit. 4 fragments made for future use.

---
+++ app/src/main/java/com/example/thesis/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.thesis.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Reports Fragment"
+    }
+    val text: LiveData<String> = _text
+}
