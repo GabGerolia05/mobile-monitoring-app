ce70d0bd96d3394f96605e299e5a8d10950b2e8e
GEROLIA_JohnGabriel_BSIT32E1 <johngabrielgerolia05@gmail.com>
2024-08-26 19:20:15 +0800

    notif template

--- app/src/main/java/com/example/thesis/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/thesis/ui/notifications/NotificationsFragment.kt
@@ -28,10 +28,6 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        settingsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
